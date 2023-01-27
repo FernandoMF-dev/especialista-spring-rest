07b8f5a0b8fad9eae69528ff7b1b88e90f532111
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-01-27 17:25:45 -0300

    Criando projeto

---
+++ algafood_fmf_alpha/src/main/java/com/fmf/algafood/AlgafoodFmfAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.fmf.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodFmfAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodFmfAlphaApplication.class, args);
+	}
+
+}
---
+++ algafood_fmf_alpha/src/test/java/com/fmf/algafood/AlgafoodFmfAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmf.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodFmfAlphaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
