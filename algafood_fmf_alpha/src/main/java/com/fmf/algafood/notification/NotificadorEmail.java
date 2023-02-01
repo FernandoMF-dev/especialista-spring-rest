aabbbbed3ee4da88231f601c1237db73f15043fe
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-02-01 19:21:22 -0300

    Acessando propriedades com @ConfigurationProperties

--- algafood_fmf_alpha/src/main/java/com/fmf/algafood/notification/NotificadorEmail.java
+++ algafood_fmf_alpha/src/main/java/com/fmf/algafood/notification/NotificadorEmail.java
@@ -3,7 +3,8 @@ package com.fmf.algafood.notification;
 import com.fmf.algafood.annotations.TipoDoNotificador;
 import com.fmf.algafood.enums.NivelUrgencia;
 import com.fmf.algafood.model.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import com.fmf.algafood.properties.NotificadorProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +13,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	@Value("${notificador.email.porta-servidor}")
-	private Integer porta;
+	@Autowired
+	private NotificadorProperties properties;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("HOST: " + host);
-		System.out.println("PORTA: " + porta);
+		System.out.println("HOST: " + properties.getHostServidor());
+		System.out.println("PORTA: " + properties.getPortaServidor());
 
 		System.out.printf("Notificando %s através do e-mail %s: %s%n",
 				cliente.getNome(), cliente.getEmail(), mensagem);
