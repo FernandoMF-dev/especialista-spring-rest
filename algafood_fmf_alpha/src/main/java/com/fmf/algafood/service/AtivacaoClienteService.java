14f94e5a5f9731f260e01e3d2724cce2d85a08e6
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-02-01 14:38:31 -0300

    Usando @Autowired para definir ponto de injeção de dependencia

--- algafood_fmf_alpha/src/main/java/com/fmf/algafood/service/AtivacaoClienteService.java
+++ algafood_fmf_alpha/src/main/java/com/fmf/algafood/service/AtivacaoClienteService.java
@@ -2,14 +2,20 @@ package com.fmf.algafood.service;
 
 import com.fmf.algafood.model.Cliente;
 import com.fmf.algafood.notification.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
-	private final Notificador notificador;
+	private Notificador notificador;
 
+	// Em uma classe com vários construtores disponíveis, essa anotação pode dizer ao Spring em qual deles fazer a injeção de dependencia
+	@Autowired
 	public AtivacaoClienteService(Notificador notificador) {
 		this.notificador = notificador;
+	}
+
+	public AtivacaoClienteService(String exemplo) {
 	}
 
 	public void ativar(Cliente cliente) {
