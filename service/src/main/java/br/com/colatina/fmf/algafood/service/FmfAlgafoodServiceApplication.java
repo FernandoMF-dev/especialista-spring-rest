820234dd28f72009da0024ed937264a190db8310
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-02-02 13:35:38 -0300

    Criando novo projeto para o curso do especialista spring REST para a aula 3.3 em diante

---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/FmfAlgafoodServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.colatina.fmf.algafood.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmfAlgafoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmfAlgafoodServiceApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/br/com/colatina/fmf/algafood/service/FmfAlgafoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.colatina.fmf.algafood.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmfAlgafoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
