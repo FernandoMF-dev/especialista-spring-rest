26997cab9ef9d41face3ed2d8807dd625c5231c9
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-02-02 13:19:26 -0300

    Descartando projeto para aulas

--- algafood_fmf_alpha/src/main/java/com/fmf/algafood/enums/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.fmf.algafood.enums;
-
-public enum NivelUrgencia {
-	URGENTE,
-	NORMAL
-}
--- algafood_fmf_alpha/src/main/java/com/fmf/algafood/notification/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.fmf.algafood.notification;
-
-import com.fmf.algafood.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
