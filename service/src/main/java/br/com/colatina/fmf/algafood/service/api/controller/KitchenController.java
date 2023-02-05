143cf48b8988179e8531ed9d2dfc0c7686da22cb
Fernando Martins Ferreira <fernando.ferreira@basis.com.br>
2023-02-05 16:29:36 -0300

    Modelando e implementando a atualização de recursos com PUT

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/KitchenController.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/KitchenController.java
@@ -5,12 +5,14 @@ import br.com.colatina.fmf.algafood.service.domain.model.Kitchen;
 import br.com.colatina.fmf.algafood.service.domain.repository.KitchenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,8 +51,21 @@ public class KitchenController {
 
 	@PostMapping()
 	public ResponseEntity<Kitchen> insert(@Valid @RequestBody Kitchen entity) {
-		log.debug("REST request to insert a new kitchen: {}", entity.toString());
+		log.debug("REST request to insert a new kitchen: {}", entity);
 		return new ResponseEntity<>(kitchenRepository.save(entity), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Kitchen> insert(@Valid @RequestBody Kitchen entity, @PathVariable Long id) {
+		log.debug("REST request to update kitchen with id {}: {}", id, entity);
+
+		Optional<Kitchen> saved = kitchenRepository.findById(id);
+		if (saved.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		BeanUtils.copyProperties(entity, saved.get(), "id");
+
+		return new ResponseEntity<>(kitchenRepository.save(saved.get()), HttpStatus.OK);
 	}
 
 }
