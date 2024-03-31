b965c023d72ff6a8cbede16a2ae1be33e6e465ef
Fernando Martins Ferreira <fmartinsf@minsait.com>
2024-03-31 15:36:38 -0300

    Criando classe de serviço para métodos de pagamento

--- service/src/main/java/br/com/colatina/fmf/algafood/service/domain/repository/PaymentMethodRepository.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/repository/PaymentMethodRepository.java
@@ -4,6 +4,10 @@ import br.com.colatina.fmf.algafood.service.domain.model.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+	Optional<PaymentMethod> findByIdAndExcludedIsFalse(Long id);
+
 }
---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/PaymentMethodCrudService.java
@@ -0,0 +1,22 @@
+package br.com.colatina.fmf.algafood.service.domain.service;
+
+import br.com.colatina.fmf.algafood.service.domain.exceptions.ResourceNotFound;
+import br.com.colatina.fmf.algafood.service.domain.model.PaymentMethod;
+import br.com.colatina.fmf.algafood.service.domain.repository.PaymentMethodRepository;
+import br.com.colatina.fmf.algafood.service.domain.service.mapper.PaymentMethodMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PaymentMethodCrudService {
+	private final PaymentMethodRepository paymentMethodRepository;
+	private final PaymentMethodMapper paymentMethodMapper;
+
+	public PaymentMethod findEntityById(Long id) {
+		return paymentMethodRepository.findByIdAndExcludedIsFalse(id)
+				.orElseThrow(() -> new ResourceNotFound(String.format("Payment method %d not found", id)));
+	}
+}
