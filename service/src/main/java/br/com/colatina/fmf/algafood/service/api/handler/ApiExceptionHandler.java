5442acf078d1fc432c9648ce34b4914a5a2ccc76
Fernando Martins Ferreira <fmartinsf@minsait.com>
2024-04-08 10:28:59 -0300

    tratando a PropertyBindingException na desserialização para propriedades inexistentes ou ignoradas

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/handler/ApiExceptionHandler.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/handler/ApiExceptionHandler.java
@@ -2,9 +2,11 @@ package br.com.colatina.fmf.algafood.service.api.handler;
 
 import br.com.colatina.fmf.algafood.service.domain.exceptions.BusinessRuleException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.fasterxml.jackson.databind.exc.PropertyBindingException;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import io.micrometer.core.lang.Nullable;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
@@ -17,13 +19,28 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	private static final String ERROR_TYPE_URI = "https://fmf.algafood.com.br/";
+
+	private final Map<Class<? extends Throwable>, InvalidBodyFormatHandler> rootExceptionsHandlers;
+
+	public ApiExceptionHandler() {
+		this.rootExceptionsHandlers = new HashMap<>();
+
+		this.rootExceptionsHandlers.put(InvalidFormatException.class,
+				(rootCause, headers, status, request) -> handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request));
+		this.rootExceptionsHandlers.put(IgnoredPropertyException.class,
+				(rootCause, headers, status, request) -> handlePropertyBindingException((IgnoredPropertyException) rootCause, headers, status, request));
+		this.rootExceptionsHandlers.put(UnrecognizedPropertyException.class,
+				(rootCause, headers, status, request) -> handlePropertyBindingException((UnrecognizedPropertyException) rootCause, headers, status, request));
+	}
 
 	@ExceptionHandler(BusinessRuleException.class)
 	public ResponseEntity<Object> handleBusinessRuleException(BusinessRuleException ex, WebRequest request) {
@@ -36,9 +53,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		Throwable rootCause = ExceptionUtils.getRootCause(ex);
-
-		if (rootCause instanceof InvalidFormatException) {
-			return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+		if (this.rootExceptionsHandlers.containsKey(rootCause.getClass())) {
+			return this.rootExceptionsHandlers.get(rootCause.getClass()).handle(rootCause, headers, status, request);
 		}
 
 		ApiErrorType type = ApiErrorType.MESSAGE_BODY_NOT_READABLE;
@@ -66,15 +82,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
-	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-		ApiErrorType type = ApiErrorType.MESSAGE_BODY_NOT_READABLE;
-		String propertyPath = ex.getPath().stream().map(JsonMappingException.Reference::getFieldName).collect(Collectors.joining("."));
-		String detail = String.format("Property '%s' has been assigned the value '%s', which is an invalid type. Correct and enter a value compatible with type '%s'",
-				propertyPath, ex.getValue(), ex.getTargetType().getSimpleName());
-		ApiErrorResponse body = createApiErrorResponseBuilder(status, type, detail).build();
-		return handleExceptionInternal(ex, body, headers, status, request);
-	}
-
 	private ApiErrorResponse.ApiErrorResponseBuilder createApiErrorResponseBuilder(HttpStatus status, ApiErrorType type, String detail) {
 		return ApiErrorResponse.builder()
 				.status(status.value())
@@ -82,5 +89,32 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.type(ERROR_TYPE_URI + type.getPath())
 				.title(type.getTitle())
 				.detail(detail);
+	}
+
+	private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ApiErrorType type = ApiErrorType.MESSAGE_BODY_NOT_READABLE;
+		String propertyPath = formatPropertyPath(ex.getPath());
+		String detail = String.format("Property '%s' has been assigned the value '%s', which is an invalid type. Correct and enter a value compatible with type '%s'",
+				propertyPath, ex.getValue(), ex.getTargetType().getSimpleName());
+		ApiErrorResponse body = createApiErrorResponseBuilder(status, type, detail).build();
+		return handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+	private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ApiErrorType type = ApiErrorType.MESSAGE_BODY_NOT_READABLE;
+		String propertyPath = formatPropertyPath(ex.getPath());
+		String detail = String.format("Property '%s' does not exist. Check for possible error or remove it entirely, and then try again.", propertyPath);
+		ApiErrorResponse body = createApiErrorResponseBuilder(status, type, detail).build();
+		return handleExceptionInternal(ex, body, headers, status, request);
+	}
+
+	private String formatPropertyPath(List<JsonMappingException.Reference> path) {
+		return path.stream()
+				.map(JsonMappingException.Reference::getFieldName)
+				.collect(Collectors.joining("."));
+	}
+
+	private interface InvalidBodyFormatHandler {
+		ResponseEntity<Object> handle(Throwable rootCause, HttpHeaders headers, HttpStatus status, WebRequest request);
 	}
 }
