3e564a021b141a82044fa6b1bb11e817f7c109e4
Fernando Martins Ferreira <fmartinsf@minsait.com>
2024-04-20 15:47:18 -0300

    Implementando 'AddressFactory' para testes automatizados

---
+++ service/src/test/java/br/com/colatina/fmf/algafood/service/factory/AddressFactory.java
@@ -0,0 +1,23 @@
+package br.com.colatina.fmf.algafood.service.factory;
+
+import br.com.colatina.fmf.algafood.service.domain.model.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressFactory {
+	@Autowired
+	CityFactory cityFactory;
+
+	public Address createEntity() {
+		Address address = new Address();
+		address.setCep(String.valueOf(System.currentTimeMillis()).substring(0, 5) + "-" + String.valueOf(System.currentTimeMillis()).substring(0, 3));
+		address.setPublicSpace(String.format("Public Space %d", System.currentTimeMillis()));
+		address.setStreetNumber(String.format("Street Number %d", System.currentTimeMillis()));
+		address.setComplement(String.format("Complement %d", System.currentTimeMillis()));
+		address.setDistrict(String.format("District %d", System.currentTimeMillis()));
+		address.setCity(cityFactory.createAndPersist());
+		return address;
+	}
+
+}
