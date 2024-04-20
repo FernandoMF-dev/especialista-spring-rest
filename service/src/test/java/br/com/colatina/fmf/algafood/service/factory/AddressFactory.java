66209003f2bfb7b4d05f24b89c6d2dd78692fbdd
Fernando Martins Ferreira <fmartinsf@minsait.com>
2024-04-20 16:27:10 -0300

    Implementando 'RestaurantFactory' para testes automatizados

--- service/src/test/java/br/com/colatina/fmf/algafood/service/factory/AddressFactory.java
+++ service/src/test/java/br/com/colatina/fmf/algafood/service/factory/AddressFactory.java
@@ -13,7 +13,7 @@ public class AddressFactory {
 		Address address = new Address();
 		address.setCep(String.valueOf(System.currentTimeMillis()).substring(0, 5) + "-" + String.valueOf(System.currentTimeMillis()).substring(0, 3));
 		address.setPublicSpace(String.format("Public Space %d", System.currentTimeMillis()));
-		address.setStreetNumber(String.format("Street Number %d", System.currentTimeMillis()));
+		address.setStreetNumber(String.format("Street Number %d", System.currentTimeMillis()).substring(0, 20));
 		address.setComplement(String.format("Complement %d", System.currentTimeMillis()));
 		address.setDistrict(String.format("District %d", System.currentTimeMillis()));
 		address.setCity(cityFactory.createAndPersist());
---
+++ service/src/test/java/br/com/colatina/fmf/algafood/service/factory/RestaurantFactory.java
@@ -0,0 +1,53 @@
+package br.com.colatina.fmf.algafood.service.factory;
+
+import br.com.colatina.fmf.algafood.service.domain.model.Restaurant;
+import br.com.colatina.fmf.algafood.service.domain.repository.RestaurantRepository;
+import br.com.colatina.fmf.algafood.service.domain.service.RestaurantCrudService;
+import br.com.colatina.fmf.algafood.service.domain.service.dto.RestaurantDto;
+import br.com.colatina.fmf.algafood.service.domain.service.mapper.RestaurantMapper;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RestaurantFactory extends BaseEntityFactory<Restaurant> {
+	@Autowired
+	RestaurantMapper restaurantMapper;
+	@Autowired
+	RestaurantCrudService restaurantCrudService;
+	@Autowired
+	RestaurantRepository restaurantRepository;
+
+	@Autowired
+	AddressFactory addressFactory;
+	@Autowired
+	PaymentMethodFactory paymentMethodFactory;
+	@Autowired
+	CuisineFactory cuisineFactory;
+
+	@Override
+	public Restaurant createEntity() {
+		Restaurant restaurant = new Restaurant();
+		restaurant.setName(String.format("Restaurant %d", System.currentTimeMillis()));
+		restaurant.setFreightFee(RandomUtils.nextDouble(0.0, 20.0));
+		restaurant.setActive(Boolean.TRUE);
+		restaurant.setCuisine(cuisineFactory.createAndPersist());
+		restaurant.setAddress(addressFactory.createEntity());
+		restaurant.setPaymentMethods(List.of(paymentMethodFactory.createAndPersist()));
+		return restaurant;
+	}
+
+	@Override
+	protected Restaurant persist(Restaurant entity) {
+		RestaurantDto dto = restaurantMapper.toDto(entity);
+		dto = restaurantCrudService.insert(dto);
+		return restaurantMapper.toEntity(dto);
+	}
+
+	@Override
+	public Restaurant getById(Long id) {
+		return restaurantRepository.findById(id).orElse(null);
+	}
+}
