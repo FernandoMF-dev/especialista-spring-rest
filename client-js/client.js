6d9336049c7118dbb422a2d43d19366403731724
Fernando Martins Ferreira <fmartinsf@minsait.com>
2024-07-13 15:05:34 -0300

    Implementando um formulário de cadastro e fazendo requisição POST com JavaScript.
    OBS: Eu estou fazendo speedrun deste módulo no curso porque não estou interessado.

--- client-js/client.js
+++ client-js/client.js
@@ -9,6 +9,35 @@ function consultar() {
 	});
 }
 
+function cadastrar() {
+	const formaPagamentoJson = JSON.stringify({
+		"description": $("#campo-descricao").val()
+	});
+
+	console.log(formaPagamentoJson);
+
+	$.ajax({
+		url: "http://localhost:8080/api/payment-methods",
+		type: "POST",
+		data: formaPagamentoJson,
+		contentType: "application/json",
+
+		success: function(response) {
+			alert("Forma de pagamento adicionada!");
+			consultar();
+		},
+
+		error: function(error) {
+			if (error.status === 400) {
+				const problem = JSON.parse(error.responseText);
+				alert(problem.userMessage);
+			} else {
+				alert("Erro ao cadastrar forma de pagamento!");
+			}
+		}
+	});
+}
+
 function preencherTabela(formasPagamento) {
 	$("#tabela tbody tr").remove();
 
@@ -26,3 +55,4 @@ function preencherTabela(formasPagamento) {
 
 
 $("#btn-consultar").click(consultar);
+$("#btn-cadastrar").click(cadastrar);
