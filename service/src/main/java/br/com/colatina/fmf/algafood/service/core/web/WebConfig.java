eff8e84c7fc9ca7892729020bc12de2f494c5324
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-17 17:45:26 -0300

    Implementando requisições condicionais com Shallow ETags

--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/web/WebConfig.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/web/WebConfig.java
@@ -1,9 +1,13 @@
 package br.com.colatina.fmf.algafood.service.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
 				.allowedOrigins(CorsConfiguration.ALL) // Habilita CORS para todas as URLs da aplicação
 				.allowedMethods("*") // Habilita todos os métodos HTTP (get, post, put, delete, etc)
 				.maxAge(30L * 60L); // Determina o tempo (em segundos) que o navegador deve armazenar a configuração CORS em cache
+	}
+
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
