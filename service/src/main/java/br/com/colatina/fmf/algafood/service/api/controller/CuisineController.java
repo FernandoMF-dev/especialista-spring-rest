784133e98539ae3470206bff35450c3276f39c4d
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-20 16:02:58 -0300

    Descrevendo as operações de endpoints na documentação

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/CuisineController.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/CuisineController.java
@@ -5,6 +5,7 @@ import br.com.colatina.fmf.algafood.service.core.openapi.SpringFoxControllerTags
 import br.com.colatina.fmf.algafood.service.domain.service.CuisineCrudService;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.CuisineDto;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -30,42 +31,49 @@ import java.util.List;
 public class CuisineController {
 	private final CuisineCrudService cuisineCrudService;
 
+	@ApiOperation("Find a list of all available cuisines")
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<CuisineDto>> findAll() {
 		log.debug("REST request to find all cuisines");
 		return new ResponseEntity<>(cuisineCrudService.findAll(), HttpStatus.OK);
 	}
 
+	@ApiOperation("Find a list of all available cuisines")
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public ResponseEntity<CuisinesXmlWrapper> findAllXml() {
 		log.debug("REST request to find all cuisines with the response on the XML format");
 		return new ResponseEntity<>(cuisineCrudService.findAllXml(), HttpStatus.OK);
 	}
 
+	@ApiOperation("Find a cuisine by its ID")
 	@GetMapping("/{id}")
 	public ResponseEntity<CuisineDto> findById(@PathVariable Long id) {
 		log.debug("REST request to find the cuisine with ID: {}", id);
 		return new ResponseEntity<>(cuisineCrudService.findDtoById(id), HttpStatus.OK);
 	}
 
+	@ApiOperation("Find the first available cuisine it can")
 	@GetMapping("/first")
 	public ResponseEntity<CuisineDto> findFirst() {
 		log.debug("REST request to find the first cuisine it can");
 		return new ResponseEntity<>(cuisineCrudService.findFirst(), HttpStatus.OK);
 	}
 
+	@ApiOperation("Insert a new cuisine")
 	@PostMapping()
 	public ResponseEntity<CuisineDto> insert(@Valid @RequestBody CuisineDto dto) {
 		log.debug("REST request to insert a new cuisine: {}", dto);
 		return new ResponseEntity<>(cuisineCrudService.insert(dto), HttpStatus.CREATED);
 	}
 
+	@ApiOperation("Update a cuisine by its ID")
 	@PutMapping("/{id}")
 	public ResponseEntity<CuisineDto> update(@Valid @RequestBody CuisineDto dto, @PathVariable Long id) {
 		log.debug("REST request to update cuisine with id {}: {}", id, dto);
 		return new ResponseEntity<>(cuisineCrudService.update(dto, id), HttpStatus.OK);
 	}
 
+	@ApiOperation("Delete a cuisine by its ID")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		log.debug("REST request to delete cuisine with id {}", id);
