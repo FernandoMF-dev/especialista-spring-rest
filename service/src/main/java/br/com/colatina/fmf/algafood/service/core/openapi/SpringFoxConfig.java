a8d5a941e12eade383ebca0ec2405162961828ae
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-21 16:23:53 -0300

    Descrevendo tags na documentação e associando com controllers

--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringFoxConfig.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,6 @@ package br.com.colatina.fmf.algafood.service.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,14 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
-				.build();
+				.apis(RequestHandlerSelectors.basePackage("br.com.colatina.fmf.algafood.service.api.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo())
+				.tags(new Tag(SpringFoxControllerTags.CUISINES, "Operations related to cuisines"));
 	}
 
 	public ApiInfo apiInfo() {
