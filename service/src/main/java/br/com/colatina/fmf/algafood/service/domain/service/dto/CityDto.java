ecda491023898e126e40cbde5190a73453459fd9
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-21 15:59:02 -0300

    Descrevendo modelos de representações e suas propriedades

--- service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/CityDto.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/CityDto.java
@@ -1,6 +1,8 @@
 package br.com.colatina.fmf.algafood.service.domain.service.dto;
 
 import br.com.colatina.fmf.algafood.service.core.validation.ValidationGroups;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,18 +16,23 @@ import javax.validation.groups.ConvertGroup;
 import java.io.Serializable;
 import java.util.Objects;
 
+@ApiModel(value = "City", description = "Representation model for city")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 public class CityDto implements Serializable {
+	@ApiModelProperty(value = "Unique ID of the city", example = "1")
 	@NotNull(message = "city.id.not_null", groups = ValidationGroups.RequiredCity.class)
 	private Long id;
+	@ApiModelProperty(value = "Acronym of the city", example = "VIX")
 	@NotNull(message = "city.acronym.not_null")
 	@Size(max = 5, message = "city.acronym.max_size")
 	private String acronym;
+	@ApiModelProperty(value = "Full name of the city", example = "Vitória")
 	@NotBlank(message = "city.name.not_blank")
 	private String name;
+	@ApiModelProperty(value = "State on which the city is located")
 	@NotNull(message = "city.state.not_null")
 	@ConvertGroup(to = ValidationGroups.RequiredState.class)
 	@Valid
--- service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/StateDto.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/StateDto.java
@@ -1,6 +1,8 @@
 package br.com.colatina.fmf.algafood.service.domain.service.dto;
 
 import br.com.colatina.fmf.algafood.service.core.validation.ValidationGroups;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +15,21 @@ import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Objects;
 
+@ApiModel(value = "State", description = "Representation model for state")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class StateDto implements Serializable {
+	@ApiModelProperty(value = "Unique ID of the state", example = "1")
 	@NotNull(message = "state.id.not_null", groups = ValidationGroups.RequiredState.class)
 	private Long id;
+	@ApiModelProperty(value = "Acronym of the state", example = "ES")
 	@NotBlank(message = "state.acronym.not_null")
 	@Size(max = 2, message = "state.acronym.max_size")
 	private String acronym;
+	@ApiModelProperty(value = "Full name of the state", example = "Espírito Santo")
 	@NotBlank(message = "state.name.not_blank")
 	private String name;
 
