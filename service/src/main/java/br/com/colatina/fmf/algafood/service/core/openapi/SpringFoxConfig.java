324d9b59e44f75b4ac6c5c7ea9f96d02e660901b
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-27 15:46:42 -0300

    Referenciando modelo de representação de problema com códigos de status de erro

--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringFoxConfig.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringFoxConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -18,6 +20,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
 @EnableSwagger2
@@ -53,53 +56,66 @@ public class SpringFoxConfig {
 	// <editor-fold defaultstate="collapsed" desc="Global response messages">
 	private List<Response> globalGetResponseMessages() {
 		return List.of(
-				newResponseMessage(HttpStatus.NOT_FOUND),
-				newResponseMessage(HttpStatus.NOT_ACCEPTABLE),
-				newResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR)
+				newErrorResponseMessage(HttpStatus.NOT_FOUND, true),
+				newErrorResponseMessage(HttpStatus.NOT_ACCEPTABLE, false),
+				newErrorResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, true)
 		);
 	}
 
 	private List<Response> globalPostResponseMessages() {
 		return List.of(
-				newResponseMessage(HttpStatus.BAD_REQUEST),
-				newResponseMessage(HttpStatus.NOT_ACCEPTABLE),
-				newResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE),
-				newResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR)
+				newErrorResponseMessage(HttpStatus.BAD_REQUEST, true),
+				newErrorResponseMessage(HttpStatus.NOT_ACCEPTABLE, false),
+				newErrorResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE, false),
+				newErrorResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, true)
 		);
 	}
 
 	private List<Response> globalPutResponseMessages() {
 		return List.of(
-				newResponseMessage(HttpStatus.BAD_REQUEST),
-				newResponseMessage(HttpStatus.NOT_FOUND),
-				newResponseMessage(HttpStatus.NOT_ACCEPTABLE),
-				newResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE),
-				newResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR)
+				newErrorResponseMessage(HttpStatus.BAD_REQUEST, true),
+				newErrorResponseMessage(HttpStatus.NOT_FOUND, true),
+				newErrorResponseMessage(HttpStatus.NOT_ACCEPTABLE, false),
+				newErrorResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE, false),
+				newErrorResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, true)
 		);
 	}
 
 	private List<Response> globalPatchResponseMessages() {
 		return List.of(
-				newResponseMessage(HttpStatus.NOT_FOUND),
-				newResponseMessage(HttpStatus.BAD_REQUEST),
-				newResponseMessage(HttpStatus.NOT_ACCEPTABLE),
-				newResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE),
-				newResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR)
+				newErrorResponseMessage(HttpStatus.NOT_FOUND, true),
+				newErrorResponseMessage(HttpStatus.BAD_REQUEST, true),
+				newErrorResponseMessage(HttpStatus.NOT_ACCEPTABLE, false),
+				newErrorResponseMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE, false),
+				newErrorResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, true)
 		);
 	}
 
 	private List<Response> globalDeleteResponseMessages() {
 		return List.of(
-				newResponseMessage(HttpStatus.BAD_REQUEST),
-				newResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR)
+				newErrorResponseMessage(HttpStatus.BAD_REQUEST, true),
+				newErrorResponseMessage(HttpStatus.INTERNAL_SERVER_ERROR, true)
 		);
 	}
 
-	private Response newResponseMessage(HttpStatus httpStatus) {
-		return new ResponseBuilder()
+	private Response newErrorResponseMessage(HttpStatus httpStatus, boolean doesReturnBody) {
+		var responseBuilder = new ResponseBuilder()
 				.code(String.valueOf(httpStatus.value()))
-				.description(httpStatus.getReasonPhrase())
-				.build();
+				.description(httpStatus.getReasonPhrase());
+
+		if (doesReturnBody) {
+			responseBuilder
+					.representation(MediaType.APPLICATION_JSON)
+					.apply(getProblemaModelReference());
+		}
+
+		return responseBuilder.build();
+	}
+
+	private Consumer<RepresentationBuilder> getProblemaModelReference() {
+		return r -> r.model(m -> m.name("ApiErrorResponse")
+				.referenceModel(ref -> ref.key(k -> k.qualifiedModelName(
+						q -> q.name("ApiErrorResponse").namespace("br.com.colatina.fmf.algafood.service.api.handler")))));
 	}
 	// </editor-fold>
 }
