16950e96ba54cbb5c7cfed16d32624308b793d33
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-07-27 16:59:28 -0300

    Desacoplando anotações do Swagger de classe controladora

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/CuisineController.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/CuisineController.java
@@ -1,16 +1,9 @@
 package br.com.colatina.fmf.algafood.service.api.controller;
 
-import br.com.colatina.fmf.algafood.service.api.handler.ApiErrorResponse;
+import br.com.colatina.fmf.algafood.service.api.controller.documentation.CuisineControllerDocumentation;
 import br.com.colatina.fmf.algafood.service.api.model.CuisinesXmlWrapper;
-import br.com.colatina.fmf.algafood.service.core.openapi.SpringFoxControllerTags;
 import br.com.colatina.fmf.algafood.service.domain.service.CuisineCrudService;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.CuisineDto;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -28,70 +21,58 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-@Api(tags = SpringFoxControllerTags.CUISINES)
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/cuisines")
-public class CuisineController {
+public class CuisineController implements CuisineControllerDocumentation {
 	private final CuisineCrudService cuisineCrudService;
 
-	@ApiOperation("Find a list of all available cuisines")
+	@Override
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<CuisineDto>> findAll() {
 		log.debug("REST request to find all cuisines");
 		return new ResponseEntity<>(cuisineCrudService.findAll(), HttpStatus.OK);
 	}
 
-	@ApiOperation("Find a list of all available cuisines")
+	@Override
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public ResponseEntity<CuisinesXmlWrapper> findAllXml() {
 		log.debug("REST request to find all cuisines with the response on the XML format");
 		return new ResponseEntity<>(cuisineCrudService.findAllXml(), HttpStatus.OK);
 	}
 
-	@ApiOperation("Find a cuisine by its ID")
-	@ApiResponse(responseCode = "400", description = "Invalid Cuisine ID", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
-	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	@Override
 	@GetMapping("/{id}")
-	public ResponseEntity<CuisineDto> findById(@ApiParam(value = "ID of a available cuisine", example = "1") @PathVariable Long id) {
+	public ResponseEntity<CuisineDto> findById(@PathVariable Long id) {
 		log.debug("REST request to find the cuisine with ID: {}", id);
 		return new ResponseEntity<>(cuisineCrudService.findDtoById(id), HttpStatus.OK);
 	}
 
-	@ApiOperation("Find the first available cuisine it can")
-	@ApiResponse(responseCode = "404", description = "No cuisine found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	@Override
 	@GetMapping("/first")
 	public ResponseEntity<CuisineDto> findFirst() {
 		log.debug("REST request to find the first cuisine it can");
 		return new ResponseEntity<>(cuisineCrudService.findFirst(), HttpStatus.OK);
 	}
 
-	@ApiOperation("Insert a new cuisine")
-	@ApiResponse(responseCode = "201", description = "Cuisine created", content = @Content(schema = @Schema(implementation = CuisineDto.class)))
+	@Override
 	@PostMapping()
-	public ResponseEntity<CuisineDto> insert(@ApiParam(name = "body", value = "Cuisine data to create")
-											 @Valid @RequestBody CuisineDto dto) {
+	public ResponseEntity<CuisineDto> insert(@Valid @RequestBody CuisineDto dto) {
 		log.debug("REST request to insert a new cuisine: {}", dto);
 		return new ResponseEntity<>(cuisineCrudService.insert(dto), HttpStatus.CREATED);
 	}
 
-	@ApiOperation("Update a cuisine by its ID")
-	@ApiResponse(responseCode = "200", description = "Cuisine updated", content = @Content(schema = @Schema(implementation = CuisineDto.class)))
-	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	@Override
 	@PutMapping("/{id}")
-	public ResponseEntity<CuisineDto> update(@ApiParam(value = "ID of a available cuisine to update", example = "1") @PathVariable Long id,
-											 @ApiParam(name = "body", value = "Cuisine data to update")
-											 @Valid @RequestBody CuisineDto dto) {
+	public ResponseEntity<CuisineDto> update(@PathVariable Long id, @Valid @RequestBody CuisineDto dto) {
 		log.debug("REST request to update cuisine with id {}: {}", id, dto);
 		return new ResponseEntity<>(cuisineCrudService.update(dto, id), HttpStatus.OK);
 	}
 
-	@ApiOperation("Delete a cuisine by its ID")
-	@ApiResponse(responseCode = "204", description = "Cuisine deleted")
-	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	@Override
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@ApiParam(value = "ID of a available cuisine to delete", example = "1") @PathVariable Long id) {
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		log.debug("REST request to delete cuisine with id {}", id);
 		cuisineCrudService.delete(id);
 		return ResponseEntity.noContent().build();
---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/controller/documentation/CuisineControllerDocumentation.java
@@ -0,0 +1,48 @@
+package br.com.colatina.fmf.algafood.service.api.controller.documentation;
+
+import br.com.colatina.fmf.algafood.service.api.handler.ApiErrorResponse;
+import br.com.colatina.fmf.algafood.service.api.model.CuisinesXmlWrapper;
+import br.com.colatina.fmf.algafood.service.core.openapi.SpringFoxControllerTags;
+import br.com.colatina.fmf.algafood.service.domain.service.dto.CuisineDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Api(tags = SpringFoxControllerTags.CUISINES)
+public interface CuisineControllerDocumentation {
+	@ApiOperation("Find a list of all available cuisines")
+	ResponseEntity<List<CuisineDto>> findAll();
+
+	@ApiOperation("Find a list of all available cuisines")
+	ResponseEntity<CuisinesXmlWrapper> findAllXml();
+
+	@ApiOperation("Find a cuisine by its ID")
+	@ApiResponse(responseCode = "400", description = "Invalid Cuisine ID", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	ResponseEntity<CuisineDto> findById(@ApiParam(value = "ID of a available cuisine", example = "1") Long id);
+
+	@ApiOperation("Find the first available cuisine it can")
+	@ApiResponse(responseCode = "404", description = "No cuisine found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	ResponseEntity<CuisineDto> findFirst();
+
+	@ApiOperation("Insert a new cuisine")
+	@ApiResponse(responseCode = "201", description = "Cuisine created", content = @Content(schema = @Schema(implementation = CuisineDto.class)))
+	ResponseEntity<CuisineDto> insert(@ApiParam(name = "body", value = "Cuisine data to create") CuisineDto dto);
+
+	@ApiOperation("Update a cuisine by its ID")
+	@ApiResponse(responseCode = "200", description = "Cuisine updated", content = @Content(schema = @Schema(implementation = CuisineDto.class)))
+	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	ResponseEntity<CuisineDto> update(@ApiParam(value = "ID of a available cuisine to update", example = "1") Long id,
+									  @ApiParam(name = "body", value = "Cuisine data to update") CuisineDto dto);
+
+	@ApiOperation("Delete a cuisine by its ID")
+	@ApiResponse(responseCode = "204", description = "Cuisine deleted")
+	@ApiResponse(responseCode = "404", description = "Cuisine not found", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+	ResponseEntity<Void> delete(@ApiParam(value = "ID of a available cuisine to delete", example = "1") Long id);
+}
