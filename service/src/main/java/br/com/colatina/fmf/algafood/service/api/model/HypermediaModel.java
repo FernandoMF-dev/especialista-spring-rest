41839ca67c94f0c1046d70323b27b9097ac9b014
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-08-31 16:20:29 -0300

    Implementando o Root Entry Point da API

---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/model/HypermediaModel.java
@@ -0,0 +1,8 @@
+package br.com.colatina.fmf.algafood.service.api.model;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class HypermediaModel extends RepresentationModel<HypermediaModel> {
+	// This class is used to add hypermedia links to the response body of a REST request
+	// when the response body would be normally empty.
+}
