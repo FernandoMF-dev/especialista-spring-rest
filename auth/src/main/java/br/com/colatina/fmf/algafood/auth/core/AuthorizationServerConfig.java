f084548bd5efc7523adbf56557470830dbb34f22
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-09-29 17:45:40 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens

--- auth/src/main/java/br/com/colatina/fmf/algafood/auth/core/AuthorizationServerConfig.java
+++ auth/src/main/java/br/com/colatina/fmf/algafood/auth/core/AuthorizationServerConfig.java
@@ -1,10 +1,33 @@
 package br.com.colatina.fmf.algafood.auth.core;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationManager authenticationManager;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+				.withClient("fmf-algafood-web")
+				.secret(passwordEncoder.encode("1234567890"))
+				.authorizedGrantTypes("password")
+				.scopes("write", "read")
+				.accessTokenValiditySeconds(60 * 60 * 6);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+		endpoints.authenticationManager(authenticationManager);
+	}
 }
--- auth/src/main/java/br/com/colatina/fmf/algafood/auth/core/WebSecurityConfig.java
+++ auth/src/main/java/br/com/colatina/fmf/algafood/auth/core/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.colatina.fmf.algafood.auth.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.withUser("fmf_algafood")
 				.password(passwordEncoder().encode("1234567890"))
 				.roles("ADMIN");
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 
 	@Bean
