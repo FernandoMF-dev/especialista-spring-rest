5412a5cc3a187b6d2cee5d2eb0b7fa8057cbee50
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2024-11-16 13:18:33 -0300

    Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/security/ResourceServerConfig.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/security/ResourceServerConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -19,6 +25,25 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 				.and()
 				.oauth2ResourceServer()
 				.jwt()
+				.jwtAuthenticationConverter(jwtAuthenticationConverter())
 		;
+	}
+
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			var authorities = jwt.getClaimAsStringList("authorities");
+
+			if (Objects.isNull(authorities)) {
+				authorities = Collections.emptyList();
+			}
+
+			return authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		});
+
+		return jwtAuthenticationConverter;
 	}
 }
