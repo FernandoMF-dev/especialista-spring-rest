6130772051856c30b9750b9127e6b0cdc3c048cb
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-22 16:34:00 -0300

    Descrevendo o modelo de representação de problema

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/handler/ApiErrorResponse.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/handler/ApiErrorResponse.java
@@ -1,29 +1,47 @@
 package br.com.colatina.fmf.algafood.service.api.handler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.Instant;
 import java.util.List;
 
+@Schema(name = "ApiErrorResponse")
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiErrorResponse {
+	@Schema(example = "400")
 	private Integer status;
+
+	@Schema(example = "https://fmf.algafood.com.br/errors/constraint-violation")
 	private String type;
+
+	@Schema(example = "Constraint violation")
 	private String title;
+
+	@Schema(example = "One or more fields are invalid. Fill in correctly and try again.")
 	private String detail;
+
+	@Schema(example = "2021-08-01T11:21:50.563Z")
 	private Instant timestamp;
 
+	@Schema(example = "One or more fields are invalid. Fill in correctly and try again.")
 	private String userMessage;
+
+	@Schema(description = "List of fields or objects with errors (optional)")
 	private List<FieldError> fields;
 
+	@Schema(name = "FieldError")
 	@Getter
 	@Builder
 	public static class FieldError {
+		@Schema(example = "name")
 		private String field;
+
+		@Schema(example = "The name is required")
 		private String message;
 	}
 }
--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringDocConfig.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringDocConfig.java
@@ -1,16 +1,21 @@
 package br.com.colatina.fmf.algafood.service.core.openapi;
 
+import br.com.colatina.fmf.algafood.service.api.handler.ApiErrorResponse;
+import io.swagger.v3.core.util.AnnotationsUtils;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
@@ -20,6 +25,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import java.util.Objects;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Configuration
 @SecurityScheme(name = SpringDocUtils.SECURITY_SCHEME_NAME, type = SecuritySchemeType.OAUTH2,
@@ -45,7 +53,8 @@ public class SpringDocConfig {
 	public OpenApiCustomiser openApiCustomiser() {
 		return openApi -> openApi.getPaths()
 				.values()
-				.forEach(pathItem -> pathItem.readOperationsMap().forEach(this::mapGlobalApiResponsesPerHttpMethod));
+				.forEach(pathItem -> pathItem.readOperationsMap()
+						.forEach((httpMethod, operation) -> mapGlobalApiResponsesPerHttpMethod(httpMethod, operation, openApi.getComponents())));
 	}
 
 	private Info apiInfo() {
@@ -74,37 +83,44 @@ public class SpringDocConfig {
 		);
 	}
 
-	private void mapGlobalApiResponsesPerHttpMethod(PathItem.HttpMethod httpMethod, Operation operation) {
+	private void mapGlobalApiResponsesPerHttpMethod(PathItem.HttpMethod httpMethod, Operation operation, Components components) {
 		ApiResponses responses = operation.getResponses();
 
 		switch (httpMethod) {
 			case GET:
-				addApiResponse(HttpStatus.NOT_FOUND, responses);
-				addApiResponse(HttpStatus.NOT_ACCEPTABLE, responses);
-				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses);
+				addApiResponse(HttpStatus.NOT_FOUND, responses, components);
+				addApiResponse(HttpStatus.NOT_ACCEPTABLE, responses, null);
+				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses, components);
 				break;
 			case POST:
-				addApiResponse(HttpStatus.BAD_REQUEST, responses);
-				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses);
+				addApiResponse(HttpStatus.BAD_REQUEST, responses, components);
+				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses, components);
 				break;
 			case PUT, PATCH:
-				addApiResponse(HttpStatus.BAD_REQUEST, responses);
-				addApiResponse(HttpStatus.NOT_FOUND, responses);
-				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses);
+				addApiResponse(HttpStatus.BAD_REQUEST, responses, components);
+				addApiResponse(HttpStatus.NOT_FOUND, responses, components);
+				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses, components);
 				break;
 			case DELETE:
-				addApiResponse(HttpStatus.NOT_FOUND, responses);
-				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses);
+				addApiResponse(HttpStatus.NOT_FOUND, responses, components);
+				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses, components);
 				break;
 			default:
-				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses);
+				addApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, responses, components);
 				break;
 		}
 	}
 
-	private void addApiResponse(HttpStatus httpStatus, ApiResponses responses) {
-		ApiResponse internalServerError = new ApiResponse().description(httpStatus.getReasonPhrase());
+	private void addApiResponse(HttpStatus httpStatus, ApiResponses responses, Components components) {
 		String status = String.valueOf(httpStatus.value());
+		ApiResponse internalServerError = new ApiResponse().description(httpStatus.getReasonPhrase());
+
+		if (Objects.nonNull(components)) {
+			MediaType mediaType = new MediaType().schema(AnnotationsUtils.resolveSchemaFromType(ApiErrorResponse.class, components, null));
+			Content content = new Content().addMediaType(APPLICATION_JSON_VALUE, mediaType);
+			internalServerError.content(content);
+		}
+
 		responses.addApiResponse(status, internalServerError);
 	}
 }
