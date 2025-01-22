ee06c3364be62ef88edf6b58dbf58b160221ff2d
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-22 14:02:43 -0300

    Descrevendo parâmetros de entrada na documentação

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/CityControllerDocumentation.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/CityControllerDocumentation.java
@@ -4,6 +4,8 @@ import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocControllerTags
 import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocUtils;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.CityDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -13,17 +15,18 @@ import org.springframework.http.ResponseEntity;
 @SecurityRequirement(name = SpringDocUtils.SECURITY_SCHEME_NAME)
 public interface CityControllerDocumentation {
 	@Operation(summary = "Find a list of all available cities")
-    CollectionModel<CityDto> findAll();
+	CollectionModel<CityDto> findAll();
 
 	@Operation(summary = "Find a city by its ID")
-    ResponseEntity<CityDto> findById(Long id);
+	ResponseEntity<CityDto> findById(@Parameter(description = "ID of a city", example = "1", required = true) Long id);
 
 	@Operation(summary = "Insert a new city")
-    ResponseEntity<CityDto> insert(CityDto dto);
+	ResponseEntity<CityDto> insert(@RequestBody(description = "City data to create", required = true) CityDto dto);
 
 	@Operation(summary = "Update a city by its ID")
-    ResponseEntity<CityDto> update(Long id, CityDto dto);
+	ResponseEntity<CityDto> update(@Parameter(description = "ID of a city to update", example = "1", required = true) Long id,
+								   @RequestBody(description = "City data to update", required = true) CityDto dto);
 
 	@Operation(summary = "Delete a city by its ID")
-    ResponseEntity<Void> delete(Long id);
+	ResponseEntity<Void> delete(@Parameter(description = "ID of a city to delete", example = "1", required = true) Long id);
 }
