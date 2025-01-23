e3a7d0d9c528fa2b9cebd1f4931c47f7bf0c809f
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-23 17:26:59 -0300

    Descrevendo media type da resposta nos endpoints

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantProductPictureControllerDocumentation.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantProductPictureControllerDocumentation.java
@@ -3,6 +3,10 @@ package br.com.colatina.fmf.algafood.service.api.v1.documentation.controller;
 import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocUtils;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.ProductPictureDto;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.ProductPictureInsertDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +16,15 @@ import java.io.IOException;
 
 @SecurityRequirement(name = SpringDocUtils.SECURITY_SCHEME_NAME)
 public interface RestaurantProductPictureControllerDocumentation {
+	@Operation(summary = "Find the picture of a product from a restaurant")
+	@ApiResponse(responseCode = "200", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ProductPictureDto.class)),
+			@Content(mediaType = "image/jpeg", schema = @Schema(type = "string", format = "binary")),
+			@Content(mediaType = "image/png", schema = @Schema(type = "string", format = "binary"))
+	})
 	ResponseEntity<ProductPictureDto> findPicture(Long restaurantId, Long productId);
 
+	@Operation(summary = "Find the picture of a product from a restaurant", hidden = true)
 	ResponseEntity<InputStreamResource> getPictureFile(Long restaurantId, Long productId, String acceptHeader) throws HttpMediaTypeNotAcceptableException;
 
 	ResponseEntity<ProductPictureDto> updatePicture(Long restaurantId, Long productId, ProductPictureInsertDto picture) throws IOException;
