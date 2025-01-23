bd9af721c81fffcbff87c19ef03d2b209ca7a407
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-23 15:36:09 -0300

    Declarando as tags que serão usadas para definir os controladores na documentação do SpringDoc

--- service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringDocControllerTags.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/openapi/SpringDocControllerTags.java
@@ -5,6 +5,14 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class SpringDocControllerTags {
 	public static final String CITIES = "Cities";
+	public static final String CUISINES = "Cuisines";
+	public static final String ORDERS = "Orders";
+	public static final String PAYMENT_METHODS = "Payment Methods";
+	public static final String PRODUCTS = "Products";
 	public static final String PROFILES = "Profiles";
+	public static final String RESTAURANTS = "Restaurants";
 	public static final String ROOT = "Root";
+	public static final String STATES = "States";
+	public static final String STATISTICS = "Statistics";
+	public static final String USERS = "Users";
 }
