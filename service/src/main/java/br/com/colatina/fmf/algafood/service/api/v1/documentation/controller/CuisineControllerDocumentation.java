13e30def09c03513b23b3ef95b242b6defba7fd1
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-23 15:48:35 -0300

    Descrevendo documentação de endpoints de cozinhas

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/CuisineControllerDocumentation.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/CuisineControllerDocumentation.java
@@ -1,25 +1,42 @@
 package br.com.colatina.fmf.algafood.service.api.v1.documentation.controller;
 
 import br.com.colatina.fmf.algafood.service.api.v1.model.CuisinesXmlWrapper;
+import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocControllerTags;
 import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocUtils;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.CuisineDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = SpringDocControllerTags.CUISINES)
 @SecurityRequirement(name = SpringDocUtils.SECURITY_SCHEME_NAME)
 public interface CuisineControllerDocumentation {
+	@Operation(summary = "Find a list of all available cuisines")
 	CollectionModel<CuisineDto> findAll();
 
+	@Operation(summary = "Find a list of all available cuisines")
 	ResponseEntity<CuisinesXmlWrapper> findAllXml();
 
-	ResponseEntity<CuisineDto> findById(Long id);
+	@Operation(summary = "Find a cuisine by its ID")
+	ResponseEntity<CuisineDto> findById(@Parameter(description = "ID of a available cuisine", example = "1", required = true) Long id);
 
+	@Operation(summary = "Find the first available cuisine it can")
 	ResponseEntity<CuisineDto> findFirst();
 
-	ResponseEntity<CuisineDto> insert(CuisineDto dto);
+	@Operation(summary = "Insert a new cuisine")
+	@ApiResponse(responseCode = "201")
+	ResponseEntity<CuisineDto> insert(@RequestBody(description = "Cuisine data to create", required = true) CuisineDto dto);
 
-	ResponseEntity<CuisineDto> update(Long id, CuisineDto dto);
+	@Operation(summary = "Update a cuisine by its ID")
+	ResponseEntity<CuisineDto> update(@Parameter(description = "ID of a available cuisine to update", example = "1", required = true) Long id,
+									  @RequestBody(description = "Cuisine data to update", required = true) CuisineDto dto);
 
-	ResponseEntity<Void> delete(Long id);
+	@Operation(summary = "Delete a cuisine by its ID")
+	@ApiResponse(responseCode = "204")
+	ResponseEntity<Void> delete(@Parameter(description = "ID of a available cuisine to delete", example = "1", required = true) Long id);
 }
--- service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/CuisineDto.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/CuisineDto.java
@@ -1,6 +1,7 @@
 package br.com.colatina.fmf.algafood.service.domain.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import org.springframework.hateoas.server.core.Relation;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
+@Schema(name = "Model <Cuisine>", description = "Representation model for cuisine")
 @Relation(collectionRelation = "cuisines")
 @Getter
 @Setter
@@ -22,9 +24,11 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CuisineDto extends RepresentationModel<CuisineDto> implements Serializable {
+	@Schema(description = "ID of the cuisine", example = "1")
 	@EqualsAndHashCode.Include
 	private Long id;
 
+	@Schema(description = "Name of the cuisine", example = "Italian")
 	@NotBlank(message = "cuisine.name.not_blank")
 	private String name;
 }
