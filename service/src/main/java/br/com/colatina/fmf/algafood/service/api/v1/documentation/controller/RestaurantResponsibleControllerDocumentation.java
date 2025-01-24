8e0ec114b1d2e1078bd996905a3529f41a59e8a8
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-24 16:02:34 -0300

    Descrevendo documentação de endpoints de associação de responsáveis por restaurantes

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantResponsibleControllerDocumentation.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantResponsibleControllerDocumentation.java
@@ -1,16 +1,29 @@
 package br.com.colatina.fmf.algafood.service.api.v1.documentation.controller;
 
+import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocControllerTags;
 import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocUtils;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.AppUserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = SpringDocControllerTags.RESTAURANTS)
 @SecurityRequirement(name = SpringDocUtils.SECURITY_SCHEME_NAME)
 public interface RestaurantResponsibleControllerDocumentation {
-	CollectionModel<AppUserDto> findAll(Long restaurantId);
+	@Operation(summary = "Find all users responsible for a restaurant")
+	CollectionModel<AppUserDto> findAll(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId);
 
-	ResponseEntity<Void> associate(Long restaurantId, Long responsibleId);
+	@Operation(summary = "Associate a user as a responsible for a restaurant")
+	@ApiResponse(responseCode = "204")
+	ResponseEntity<Void> associate(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+								   @Parameter(description = "ID of the user to be associated as a responsible for the restaurant", example = "1", required = true) Long responsibleId);
 
-	ResponseEntity<Void> disassociate(Long restaurantId, Long responsibleId);
+	@Operation(summary = "Disassociate a user from being a responsible for a restaurant")
+	@ApiResponse(responseCode = "204")
+	ResponseEntity<Void> disassociate(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+									  @Parameter(description = "ID of the user to be disassociated from being a responsible for the restaurant", example = "1", required = true) Long responsibleId);
 }
