44133433be47fb520ec0c914f2f6ba5556b9280f
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-24 16:28:40 -0300

    Descrevendo documentação de endpoints de produtos

--- service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantProductControllerDocumentation.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/api/v1/documentation/controller/RestaurantProductControllerDocumentation.java
@@ -1,20 +1,39 @@
 package br.com.colatina.fmf.algafood.service.api.v1.documentation.controller;
 
+import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocControllerTags;
 import br.com.colatina.fmf.algafood.service.core.openapi.SpringDocUtils;
 import br.com.colatina.fmf.algafood.service.domain.service.dto.ProductDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = SpringDocControllerTags.PRODUCTS)
 @SecurityRequirement(name = SpringDocUtils.SECURITY_SCHEME_NAME)
 public interface RestaurantProductControllerDocumentation {
-	CollectionModel<ProductDto> findAll(Long restaurantId);
+	@Operation(summary = "Find all products for a restaurant")
+	CollectionModel<ProductDto> findAll(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId);
 
-	ResponseEntity<ProductDto> findById(Long restaurantId, Long productId);
+	@Operation(summary = "Find a product by its ID for a restaurant")
+	ResponseEntity<ProductDto> findById(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+										@Parameter(description = "ID of the product", example = "1", required = true) Long productId);
 
-	ResponseEntity<ProductDto> insert(Long restaurantId, ProductDto dto);
+	@Operation(summary = "Insert a new product for a restaurant")
+	@ApiResponse(responseCode = "201")
+	ResponseEntity<ProductDto> insert(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+									  @RequestBody(description = "Product data to create", required = true) ProductDto dto);
 
-	ResponseEntity<ProductDto> update(Long restaurantId, Long productId, ProductDto dto);
+	@Operation(summary = "Update an existing product for a restaurant")
+	ResponseEntity<ProductDto> update(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+									  @Parameter(description = "ID of the product", example = "1", required = true) Long productId,
+									  @RequestBody(description = "Product data to update", required = true) ProductDto dto);
 
-	ResponseEntity<Void> delete(Long restaurantId, Long productId);
+	@Operation(summary = "Delete a product by its ID for a restaurant")
+	@ApiResponse(responseCode = "204")
+	ResponseEntity<Void> delete(@Parameter(description = "ID of the restaurant", example = "1", required = true) Long restaurantId,
+								@Parameter(description = "ID of the product", example = "1", required = true) Long productId);
 }
--- service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/ProductDto.java
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/domain/service/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package br.com.colatina.fmf.algafood.service.domain.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import java.io.Serializable;
 
+@Schema(name = "Model <Product>", description = "Representation model for a product")
 @Relation(collectionRelation = "products")
 @Getter
 @Setter
@@ -22,23 +24,30 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto extends RepresentationModel<ProductDto> implements Serializable {
+	@Schema(description = "ID of the product", example = "1")
 	@EqualsAndHashCode.Include
 	private Long id;
 
+	@Schema(description = "Name of the product", example = "Cheeseburger")
 	@NotBlank(message = "product.name.not_blank")
 	private String name;
 
+	@Schema(description = "Description of the product", example = "A delicious cheeseburger")
 	@NotNull(message = "product.description.not_null")
 	private String description;
 
+	@Schema(description = "Price of the product", example = "9.99")
 	@NotNull(message = "product.price.not_null")
 	@PositiveOrZero(message = "product.price.positive_or_zero")
 	private Double price;
 
+	@Schema(description = "Active status of the product", example = "true")
 	@NotNull(message = "product.active.not_null")
 	private Boolean active;
 
+	@Schema(description = "ID of the restaurant", example = "1")
 	private Long restaurantId;
 
+	@Schema(description = "Name of the restaurant", example = "Burger King")
 	private String restaurantName;
 }
