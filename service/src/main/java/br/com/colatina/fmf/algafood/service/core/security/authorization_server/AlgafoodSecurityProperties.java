ead1aab2d015f5f574ac5d7f80bf74bf69644a83
Fernando Martins Ferreira <fernandomartinsferreira70mil@gmail.com>
2025-01-28 15:48:05 -0300

    Configuração inicial do Authorization Server com Access Token opaco

---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/security/authorization_server/AlgafoodSecurityProperties.java
@@ -0,0 +1,19 @@
+package br.com.colatina.fmf.algafood.service.core.security.authorization_server;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Component
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("algafood.auth")
+public class AlgafoodSecurityProperties {
+	@NotBlank
+	private String providerUrl;
+}
---
+++ service/src/main/java/br/com/colatina/fmf/algafood/service/core/security/authorization_server/AuthorizationServerConfig.java
@@ -0,0 +1,61 @@
+package br.com.colatina.fmf.algafood.service.core.security.authorization_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+public class AuthorizationServerConfig {
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthorizationServerSettings providerSettings(AlgafoodSecurityProperties properties) {
+        return AuthorizationServerSettings.builder()
+                .issuer(properties.getProviderUrl())
+                .build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient algafoodBackend = backendRegisteredClient(passwordEncoder);
+
+        return new InMemoryRegisteredClientRepository(List.of(algafoodBackend));
+    }
+
+    private RegisteredClient backendRegisteredClient(PasswordEncoder passwordEncoder) {
+        TokenSettings tokenSettings = TokenSettings.builder()
+                .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                .accessTokenTimeToLive(Duration.ofMinutes(30))
+                .build();
+
+        return RegisteredClient.withId("1")
+                .clientId("algafood-backend")
+                .clientSecret(passwordEncoder.encode("backend123"))
+                .clientAuthenticationMethods(set -> set.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC))
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .scope("READ")
+                .tokenSettings(tokenSettings)
+                .build();
+    }
+}
